3401c4659e496731c2dad21e46d858c46fbcfb07
Kılıç Ali Temiz <katemiz@gmail.com>
2025-03-14 14:01:42 +0300

    bending moment graph with mastClass

--- mastClass.js
+++ mastClass.js
@@ -2,30 +2,25 @@ class MastClass {
     constructor(data = false) {
 
         if (data) {
-
             this.data = data
-
         } else {
-
-            this.data = this.getInitialData()
-
+            this.data = defaultData // from data.js
         }
-        
-        
+
+
+        this.data.horAxisZData = [0]    // z, in mm
+        this.data.verAxisMomentData = []
+        this.data.verAxisDeflectionData = [0]
+        this.data.verAxisDeflectionData2 = [0]
+
+
+
     }
 
 
-
-
-
-
-
-
-
-
-
-
     doCalculations() {
+
+        let totalMass = 0;
 
         // HEOGHTS AND Z COORDINATES
         this.calculateHeights()
@@ -33,7 +28,7 @@ class MastClass {
         // ROOT MOMENT
         this.calculateRootMoment();
 
-        this.data.tubes.forEach((tube,index) => {
+        this.data.tubes.forEach((tube) => {
 
             // AREA
             this.calculateArea(tube)
@@ -51,20 +46,30 @@ class MastClass {
             this.calculateMoments(tube);
 
             // // TUBE M/EI VALUES
-            this.calculateM_EI(index);
+            this.calculateM_EI(tube);
 
-            // // TUBE MOMENT - AREA
-            // this.calculateMomentArea(tube);
+            // TUBE MOMENT - AREA
+            this.calculateMomentArea(tube);
 
-            // // TUBE x bar VALUE
-            // this.calculateTubeXBar(tube);
+            // TUBE x bar VALUE OF MOMENT AREA
+            this.calculateMomentAreaXBar(tube);
+
+            // Mass summation
+            totalMass += tube.massKG
         })
 
+        this.calculateDeflection()
+        this.data.totalMass = totalMass
+
+        // PREPARE GRAPH DATA : BENDING MOMENT, DEFLECTION
+        this.PrepareGraphData()
+
+        // DRAW GRAPHS
+        this.DrawBendingMomentDeflectionChart()
 
         console.log("NEW this.data",this.data)
-
-
     }
+
 
     calculateHeights() {
 
@@ -90,7 +95,7 @@ class MastClass {
 
         this.data.extendedHeight = z
         this.data.nestedHeight = nestedHeight
-        this.data.totalHeight = zTop+this.data.zOffset
+        this.data.totalHeight = z+this.data.zOffset
     }
 
 
@@ -132,196 +137,210 @@ class MastClass {
     calculateRootMoment() {
         this.data.mRootNmm = this.data.horLoad*this.data.totalHeight; // Nmm
         this.data.mRootNm = this.data.mRootNmm/1000; // Nm
-        // this.data.graph = [{tubeNo:1,z:0,m:-this.data.mRootNmm}]
     } 
 
 
     calculateMoments(tube){
 
         tube.mTop = this.data.mRootNmm*tube.zTop/this.data.totalHeight-this.data.mRootNmm; // Nmm
+        tube.mKink = this.data.mRootNmm*tube.zKink/this.data.totalHeight-this.data.mRootNmm; // Nmm
         tube.mBottom = this.data.mRootNmm*tube.zBottom/this.data.totalHeight-this.data.mRootNmm; // Nmm
-
-
-        // this.data.graph.push({
-        //     tubeNo:tube.no,z:tube.zTop,m:tube.mTop
-        // })
     } 
-
-
 
 
     calculateM_EI(tube){
 
-
-
-        if (tube.no == 1) {
-            tube.meiTop = tube.mTop/tube.ei;
-            tube.meiBottom = tube.mBottom/tube.ei;
-        }
-
-
-
-
-        // if (index <1) {
-        //     tube.meiBottom = tube.mA/tube.ei
-        // } else {    
-        //     tube.meiBottom = tube.mC/tube.ei
-        // }
-
-        // tube.moment = tube.mF/tube.ei
+        tube.meiTop = tube.mTop/tube.ei;
+        tube.meiKink = tube.mKink/tube.ei;
     } 
 
 
+    calculateMomentArea(tube){
+        tube.momentArea = (tube.zTop-tube.zBottom)*(tube.meiTop+tube.meiKink)/2;    
+    } 
 
 
-    getInitialData() {
+    calculateMomentAreaXBar(tube){
+    
+        let width = Math.abs(tube.zTop-tube.zKink)
 
-        return {
+        let mK = Math.abs(tube.meiKink)
+        let mT = Math.abs(tube.meiTop)
 
-            "horLoad":1000,
-            "zOffset": 200,
-        
-            "tubes": [
-                {
-                    "length": 2250,
-                    "od": 300,
-                    "thickness": 5,
-                    "density": 2710,
-                    'E':70E3,
-                    'wPayloadAdapter':1.2,
-                    'wFootFixedAdapter':1.2,
-                    'wFootRollerAdapter':1.5,
-                    'wHeadRollerAdapter':1.6,
-                    'hHead':70,
-                    'pAdapterHeadHeight':60,
-                    "overlap": 0,
-                    "headExtension":0,
-                },
-                {
-                    "length": 2250,
-                    "od": 280,
-                    "thickness": 4,
-                    "density": 2710,
-                    'E':70E3,
-                    'wPayloadAdapter':1.2,
-                    'wFootFixedAdapter':1.2,
-                    'wFootRollerAdapter':1.5,
-                    'wHeadRollerAdapter':1.6,
-                    'hHead':70,
-                    'pAdapterHeadHeight':60,
-                    "overlap": 500,
-                    "headExtension":250,
-                },
-                {
-                    "length": 2250,
-                    "od": 220,
-                    "thickness": 3,
-                    "density": 2710,
-                    'E':70E3,
-                    'wPayloadAdapter':1.2,
-                    'wFootFixedAdapter':1.2,
-                    'wFootRollerAdapter':1.5,
-                    'wHeadRollerAdapter':1.6,
-                    'hHead':70,
-                    'pAdapterHeadHeight':60,
-                    "overlap": 500,
-                    "headExtension":250,
-                },
-                {
-                    "length": 2250,
-                    "od": 180,
-                    "thickness": 2,
-                    "density": 2710,
-                    'E':70E3,
-                    'wPayloadAdapter':1.2,
-                    'wFootFixedAdapter':1.2,
-                    'wFootRollerAdapter':1.5,
-                    'wHeadRollerAdapter':1.6,
-                    'hHead':70,
-                    'pAdapterHeadHeight':60,
-                    "overlap": 500,
-                    "headExtension":250,
-                },
-                {
-                    "length": 2250,
-                    "od": 170,
-                    "thickness": 2,
-                    "density": 2710,
-                    'E':70E3,
-                    'wPayloadAdapter':1.2,
-                    'wFootFixedAdapter':1.2,
-                    'wFootRollerAdapter':1.5,
-                    'wHeadRollerAdapter':1.6,
-                    'hHead':70,
-                    'pAdapterHeadHeight':60,
-                    "overlap": 500,
-                    "headExtension":250,
-                },
-                {
-                    "length": 2250,
-                    "od": 160,
-                    "thickness": 2,
-                    "density": 2710,
-                    'E':70E3,
-                    'wPayloadAdapter':1.2,
-                    'wFootFixedAdapter':1.2,
-                    'wFootRollerAdapter':1.5,
-                    'wHeadRollerAdapter':1.6,
-                    'hHead':70,
-                    'pAdapterHeadHeight':60,
-                    "overlap": 500,
-                    "headExtension":250,
-                },
-        
-                {
-                    "length": 2250,
-                    "od": 160,
-                    "thickness": 2,
-                    "density": 2710,
-                    'E':70E3,
-                    'wPayloadAdapter':1.2,
-                    'wFootFixedAdapter':1.2,
-                    'wFootRollerAdapter':1.5,
-                    'wHeadRollerAdapter':1.6,
-                    'hHead':70,
-                    'pAdapterHeadHeight':60,
-                    "overlap": 500,
-                    "headExtension":250,
-                },
-        
-                {
-                    "length": 2250,
-                    "od": 160,
-                    "thickness": 2,
-                    "density": 2710,
-                    'E':70E3,
-                    'wPayloadAdapter':1.2,
-                    'wFootFixedAdapter':1.2,
-                    'wFootRollerAdapter':1.5,
-                    'wHeadRollerAdapter':1.6,
-                    'hHead':70,
-                    'pAdapterHeadHeight':60,
-                    "overlap": 500,
-                    "headExtension":0,
-                },        
-            ],
+        let totalArea = (mK+mT)*width/2
+        let rectArea = mK > mT ? mT*width : mK*width
+        let triArea = Math.abs(mT-mK)*width/2
+
+        let xbar_rectangle = width/2
+        let xbar_triangle = mK > mT ? width/3 : 2*width/3
+
+        let xbar = (rectArea*xbar_rectangle+triArea*xbar_triangle)/totalArea
+
+        tube.zXBar =tube.zBottom+xbar
+    }
+
+
+    calculateDeflection(){
+
+        let tubesCloned = structuredClone(this.data.tubes);
+        let sayac = tubesCloned.length
+        let deflectionTop;
+    
+        for (let i = sayac; i >= 1; i--) {
+    
+            deflectionTop = this.CalculateSubSystemDeflection(tubesCloned)
+            tubesCloned.pop()
+    
+            this.data.tubes[i-1].deflectionTop = deflectionTop
         }
+    }    
+
+
+    CalculateSubSystemDeflection(subTubesArray) {
+
+        let momentArmLocation = subTubesArray[subTubesArray.length-1].zTop
+    
+        let deflection = 0
+    
+        subTubesArray.forEach((t) => {
+            deflection = deflection+t.momentArea*(momentArmLocation-t.zXBar)
+        })
+        
+        return deflection
     }
 
 
 
 
+    PrepareGraphData() {
+
+
+
+        this.data.verAxisMomentData.push(-this.data.mRootNmm)
+
+
+
+        this.data.tubes.forEach((tube) => {
+
+
+
+            this.data.horAxisZData.push(tube.zTop)
+            this.data.verAxisMomentData.push(tube.mTop)
+            this.data.verAxisDeflectionData.push(tube.deflectionTop)
+            this.data.verAxisDeflectionData2.push(tube.deflectionTop-200)
 
 
 
 
 
 
+            // this.data.meiData.push([
+            //     {
+            //         z:tube.zKink,
+            //         mei:tube.meiKink
+            //     },
+
+            //     {
+            //         z:tube.zTop,
+            //         mei:tube.meiTop
+            //     },
+            // ])
+
+
+
+        })
 
 
 
 
 
 
+        console.log('bending moment data',this.data.horAxisZData,this.data.verAxisMomentData,this.data.verAxisDeflectionData)
+
+
+    }
+
+
+    DrawBendingMomentDeflectionChart() {
+
+        let TESTER = document.getElementById('bm_deflection');
+
+        let bendingMoment = {
+            x: this.data.horAxisZData,
+            y: this.data.verAxisMomentData,
+            yaxis: 'y',
+            mode: 'lines+markers',
+            name: 'Moment, Nmm'
+        };
+          
+        var TipDeflection = {
+            x: this.data.horAxisZData,
+            y: this.data.verAxisDeflectionData,
+            yaxis:'y2',
+            // fill: 'tozeroy',
+            mode: 'lines+markers',
+            name: 'Deflection, mm',
+            line: {shape: 'spline'},
+        };
+
+
+        var TipDeflection2 = {
+            x: this.data.horAxisZData,
+            y: this.data.verAxisDeflectionData2,
+            yaxis:'y3',
+            // fill: 'tozeroy',
+            mode: 'lines+markers',
+            name: 'Deflection, mm',
+            line: {shape: 'spline'},
+        };
+
+        var veri = [bendingMoment,TipDeflection,TipDeflection2];
+
+        var layout = {
+
+            title: {
+              text: 'Bending Moment and Deflection Graph'
+            },
+          
+            xaxis: {
+              title: {
+                text: 'z, Height, mm'
+              }
+            },
+          
+            yaxis: {
+              title: {
+                text: 'Moment, Nmm'
+              }
+            },
+    
+            yaxis2: {
+                title: {
+                  text: 'Deflection, mm',
+                  font: {color: 'rgb(18, 9, 110)'}
+                },
+            
+                tickfont: {color: 'rgb(248, 11, 11)'},
+                overlaying: 'y',
+                side: 'right'
+            },
+
+
+            yaxis3: {
+                title: {
+                  text: 'Deflection, mm',
+                  font: {color: 'rgb(139, 137, 165)'}
+                },
+            
+                tickfont: {color: 'rgb(11, 19, 248)'},
+                overlaying: 'y',
+                side: 'right'
+            },
+    
+            showlegend: true,
+        };
+    
+        Plotly.newPlot(TESTER, veri, layout);
+    }
 
 }
